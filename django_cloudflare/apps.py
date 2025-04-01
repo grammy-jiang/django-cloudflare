73d17c5856d5960e294f3816a9b66f47b49d5c25
grammy.jiang <grammy.jiang@gmail.com>
2025-04-01 05:40:21 +0000

    Update the code because of new hooks

--- django_cloudflare/apps.py
+++ django_cloudflare/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoCloudflareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_cloudflare'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_cloudflare"
