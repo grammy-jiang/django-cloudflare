12ff029a2a1e4ac81f00d34992227971a3705772
grammy.jiang <grammy.jiang@gmail.com>
2025-04-01 03:16:47 +0000

    Update config.settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vnj=beb1h53lh*+k1_n#ao)!ev0-pohrf4p()-*@tbmskt9)z2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
